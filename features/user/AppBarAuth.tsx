9b33e3b2e5d022a5c93a6655482042b3fc99b73c
RuslanB <menejer.r@yandex.com>
2024-02-16 19:27:45 +0300

    add themetoggler

--- features/user/AppBarAuth.tsx
+++ features/user/AppBarAuth.tsx
@@ -1,13 +1,17 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { signIn } from 'next-auth/react';
+import { signIn, useSession } from 'next-auth/react';
+import { ThemeToggler } from '@/components/ThemeToggler';
 import React from 'react'
 
 
 export default function AppBarAuth() {
+  const session = useSession();
+  console.log(session)
   return (
     <div className='flex gap-4'>
+        <ThemeToggler />
         <Button onClick={() => signIn()}>Login</Button>
         <Button onClick={() => signIn()}>Sing Up</Button>
     </div>
