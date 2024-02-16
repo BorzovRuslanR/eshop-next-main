465a861c5cdde99a8d0fee58e02f5029cd20743e
RuslanB <menejer.r@yandex.com>
2024-02-16 22:03:59 +0300

    queryKey fix

--- features/cart/CartPage.tsx
+++ features/cart/CartPage.tsx
@@ -1,6 +1,19 @@
+'use client';
+
+import { Product } from '@prisma/client';
+import { useQuery } from '@tanstack/react-query'
 import React from 'react'
 
 export default function CartPage() {
+
+    const { data: products, isLoading } = useQuery<Product[]>({
+        queryKey: ['product'],
+        queryFn: () => {
+            return fetch('/api/product').then(res => res.json());
+        }
+    })
+    if (isLoading) return <div>Loading...</div>
+    if (!products) return <div>Load error</div>
   return (
     <div>
         <h2>Cart</h2>
