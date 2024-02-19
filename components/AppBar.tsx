51d7a56342be90a69f5cc7a7da3b78df8dc89fe5
RuslanB <menejer.r@yandex.com>
2024-02-19 18:03:36 +0300

    app logo

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -1,12 +1,16 @@
 import AppBarAuth from '@/features/user/AppBarAuth'
 import Link from 'next/link'
 import React from 'react'
+import Image from 'next/image'
 
 
 export default function AppBar() {
   return (
     <header className='flex gap-4 justify-between items-center h-16 px-4 shadow-md
      shadow-slate-600 dark:shadow-slate-100'>
+        <div>
+          <Image src={'/logo2.jpg'} alt="Logo" width={100} height={100} style={{ color: 'inherit' }}/>
+        </div>
         <nav>
           <Link href={'/'} className=''>Main page</Link>
         </nav>
