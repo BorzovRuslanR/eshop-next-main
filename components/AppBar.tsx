a0a8e47d5a976d75f22f37cabe29ef0c7619997c
RuslanB <menejer.r@yandex.com>
2024-02-20 15:04:57 +0300

    create buttons for appbar

--- components/AppBar.tsx
+++ components/AppBar.tsx
@@ -6,13 +6,25 @@ import Image from 'next/image'
 
 export default function AppBar() {
   return (
-    <header className='flex gap-4 justify-between items-center h-16 px-4 shadow-md
+    <header className='flex gap-4 justify-between items-center h-16 px-4 py-2 shadow-md
      shadow-slate-600 dark:shadow-slate-100'>
         <div>
-          <Image src={'/logo2.jpg'} alt="Logo" width={100} height={100} style={{ color: 'inherit' }}/>
+          <Link href={'/'}>
+            <Image 
+              src={'/logo2.png'}
+              alt="Logo" width={100}
+              height={100}
+              style={{ color: 'inherit' }}
+            />
+          </Link>
         </div>
-        <nav>
-          <Link href={'/'} className=''>Main page</Link>
+        <nav className="flex justify-items-start items-center gap-4">
+          <div className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-32 flex items-center justify-center'>
+            <Link href="/" className="">Main page</Link>
+          </div>
+          <div className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-32 flex items-center justify-center">
+            <Link href={'/catalog'}>Catalog</Link>
+          </div>
         </nav>
         <AppBarAuth />
     </header>
